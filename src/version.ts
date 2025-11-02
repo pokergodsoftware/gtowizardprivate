0dc06c4ae28f6e92edfeb42090698ea80492a109
pokergodsoftware <dantoon200@gmail.com>
2025-11-02 04:06:47 -0300

    chore: release v1.0.3

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Versão do aplicativo - atualizada automaticamente
-export const APP_VERSION = '1.0.2';
-export const BUILD_DATE = '2025-11-02T06:47:31.933Z';
+export const APP_VERSION = '1.0.3';
+export const BUILD_DATE = '2025-11-02T07:06:47.522Z';
